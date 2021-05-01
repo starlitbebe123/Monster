528dd90388410bd98f50a9f1b025bf87dc392368
starlitbebe123 <82298568+starlitbebe123@users.noreply.github.com>
2021-05-01 21:39:17 +0800

    Initial commit

---
+++ Assets/Monster.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 1)]
+    public float speed = 3.5f;
+
+    [Header("攻擊力"), Range(0, 500)]
+    public float attack = 100f;
+
+    [Range(0, 5000)]
+    public float health = 350f;
+
+    [Header("掉落道具"), Tooltip("掉落道具")]
+    public GameObject item;
+
+    [Header("掉落道具機率"), Range(0, 1)]
+    public float itemChance = 1f;
+
+    [Header("掉落道具音效"), Tooltip("掉落道具音效")]
+
+    public AudioClip itemSfx;
+
+    AudioSource aSource;
+
+    Rigidbody2D rig;
+
+    Animator anim;
+    
+}
